453d209ac92ec0a493fd917f8037f61cf8746ebe
valentinb <valentin.bus@d2si.io>
2019-08-08 15:55:17 +0200

    [api] Add root to get wiki info

--- flask/app.py
+++ flask/app.py
@@ -1,23 +1,31 @@
 from flask import Flask, request, render_template, jsonify, Response
-from backend import wikipedia
-
+from backend.wikipedia import wikipedia
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def index():
     return render_template('bot.html')
 
 
-@app.route('/wiki_response', methods=["GET", "POST"])
+@app.route('/wiki_response', methods=["GET"])
 def response():
-    #q = resquests.get('')
-    return jsonify(wikipedia.get_all_wiki_info('cassis')
-)
+
+    query = " ".join([arg for arg in request.args])
+
+    if query != "":
+        return jsonify(wikipedia.get_all_wiki_info(query))
+    else:
+        return jsonify(wikipedia.get_all_wiki_info(query).get("error").get("info"))
+
+
 if __name__ == '__main__':
     app.run(
         debug=True,
         host='0.0.0.0', 
-        port=80
+        port=80,
     )
--- flask/backend/wikipedia/wikipedia.py
+++ flask/backend/wikipedia/wikipedia.py
@@ -14,12 +14,11 @@ def get_all_wiki_info(search):
         "action" : "opensearch",
         "search" : search, 
         "format" : "json"
-    }
+        }
 
     r = SESSION.get(url=BASE_URL, params=params)
     data = r.json()
-
     return data
-
+    
 from pprint import pprint
-pprint(get_all_wiki_info("bob"))
+pprint(get_all_wiki_info(""))
