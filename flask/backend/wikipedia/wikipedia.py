535d511933e78137910cb58ee771ca6d0b2937c3
valentinb <valentin.bus@d2si.io>
2019-09-18 10:51:48 +0200

    [backend] Add parser

--- flask/backend/wikipedia/wikipedia.py
+++ flask/backend/wikipedia/wikipedia.py
@@ -1,14 +1,35 @@
 import requests
-
+import re
 
 BASE_URL = "https://fr.wikipedia.org/w/api.php"
 SESSION = requests.session()
+
+def __parser(q):
+    """
+    Parse user element to being usable on wikipedia
+    """
+
+    q = q.lower()
+    p = re.compile(
+        "ou est \s*([^.?:;]*)$|"
+        "ou se trouve\s*([^.?:;]*)$|"
+        "l’adresse de\s*([^.?:;]*)$"
+        "ou se situe\s*([^.?:;]*)$"
+        )
+    match = p.match(q)
+
+    if match:
+        result = [group for group in match.groups() if group is not None]
+        return result
+    
+    return None
 
 
 def get_all_wiki_info(search):
     """
     Get info from wikipedia
     """
+    search = __parser(search)
 
     params = {
         "action" : "opensearch",
